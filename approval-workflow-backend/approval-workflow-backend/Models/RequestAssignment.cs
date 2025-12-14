eaae7c4338cf1273e759adeff71969a45d873bef
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 12:48:55 +0530

    Add RequestAssignment to track auditor ownership

---
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestAssignment.cs
@@ -0,0 +1,19 @@
+﻿using ApprovalWorkflow.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace approval_workflow_backend.Models
+{
+    public class RequestAssignment
+    {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        [Required]
+        public Request Request { get; set; }
+        [Required]
+        public int AuditorId { get; set; }
+        public User Auditor { get; set; }
+        [Required]
+        public DateTime AssignedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
