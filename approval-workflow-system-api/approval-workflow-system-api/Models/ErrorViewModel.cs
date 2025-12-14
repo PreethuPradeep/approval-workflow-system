b91cb9f6f1c5673deef2c57b44496cd272ab8f1d
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 11:45:03 +0530

    Initial Commit

--- approval-workflow-system-api/approval-workflow-system-api/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace approval_workflow_system_api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
