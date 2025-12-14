81e97245e056771d9b264cee5a15d1bac4a6062b
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 13:23:07 +0530

    Add RedressalContent and RequestAudit entities along with RequestAction enums

---
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestAction.cs
@@ -0,0 +1,12 @@
+﻿namespace approval_workflow_backend.Models
+{
+    public enum RequestAction
+    {
+        Submit = 1,
+        Assign = 2,
+        Approve = 3,
+        Reject = 4,
+        Redress = 5,
+        Escalate = 6
+    }
+}
---
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestAudit.cs
@@ -0,0 +1,33 @@
+﻿
+using ApprovalWorkflow.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace approval_workflow_backend.Models
+{
+    public class RequestAudit
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int RequestId { get; set; }
+        public Request MyProperty { get; set; }
+        
+        public int? RedressalId { get; set; }
+        public Redressal Redressal { get; set; }
+        [Required]
+        public int ActorId { get; set; }
+        public User Actor { get; set; }
+        [Required]
+        public string ActorRole { get; set; }
+        [Required]
+        public RequestState FromState { get; set; }
+        [Required]
+        public RequestState ToState { get; set; }
+        [Required]
+        public RequestAction Action { get; set; }
+        public string Reason { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
