bbd5badb7ee415ca12fd3cf31cc7a464068d3beb
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 13:23:25 +0530

    Reordered for easy understanding

--- approval-workflow-backend/approval-workflow-backend/Models/RequestContent.cs
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestContent.cs
@@ -7,9 +7,10 @@ namespace approval_workflow_backend.Models
     {
         [Key]
         public int RequestId { get; set; }
+        public Request Request { get; set; }
         [Required]
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
-        public Request Request { get; set; }
+
     }
 }
