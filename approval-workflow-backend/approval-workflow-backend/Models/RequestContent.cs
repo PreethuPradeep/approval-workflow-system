89a1483f2a1447f0e2f1d55207abcfa044520bd6
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 12:38:35 +0530

    Add RequestContent entity with one-to-one relationship to Request

---
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestContent.cs
@@ -0,0 +1,15 @@
+﻿using ApprovalWorkflow.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace approval_workflow_backend.Models
+{
+    public class RequestContent
+    {
+        [Key]
+        public int RequestId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Request Request { get; set; }
+    }
+}
