1b3cfb4227234e3441b00e0716fc595191d3ea32
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 12:26:29 +0530

    Add identity models

---
+++ approval-workflow-backend/approval-workflow-backend/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace approval_workflow_backend.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
