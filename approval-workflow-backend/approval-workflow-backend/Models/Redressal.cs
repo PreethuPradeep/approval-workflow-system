b89ed16a5f7e134c0fd43d2b55f91cfaf7e8be8d
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-14 13:10:04 +0530

    Add Redressal entity with controlled retry support

---
+++ approval-workflow-backend/approval-workflow-backend/Models/Redressal.cs
@@ -0,0 +1,20 @@
+﻿using approval_workflow_backend.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApprovalWorkflow.Domain.Models
+{
+    public class Redressal
+    {
+        public int Id { get; set; }
+        [Required]
+        public int RequestId { get; set; }
+        public Request Request { get; set; }
+        [Required]
+        public int RedressalCount { get; set; }
+        public bool IsActive { get; set; } = true;
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ClosedAt { get; set; }
+
+    }
+}
