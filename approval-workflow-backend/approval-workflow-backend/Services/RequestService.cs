2847f408b0f89c406f2b6fb5575fc657871f0c1b
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-17 11:02:49 +0530

    create request service

---
+++ approval-workflow-backend/approval-workflow-backend/Services/RequestService.cs
@@ -0,0 +1,17 @@
+﻿using approval_workflow_backend.Guards;
+using approval_workflow_backend.Infrastructure;
+using approval_workflow_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace approval_workflow_backend.Services
+{
+    public class RequestService
+    {
+        private readonly AppDbContext _context;
+        public RequestService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
