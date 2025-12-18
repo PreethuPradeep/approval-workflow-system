66f75ad39e64e0b44e721c99a4efd7b471252748
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-12-19 01:04:40 +0530

    Dto needed for the requestsController.cs

---
+++ approval-workflow-backend/approval-workflow-backend/Models/RequestDto.cs
@@ -0,0 +1,18 @@
+﻿namespace approval_workflow_backend.Models
+{
+    public class AssignRequestDto
+    {
+        public int AuditorId { get; set; }
+    }
+
+    public class ReasonDto
+    {
+        public string Reason { get; set; }
+    }
+
+    public class CreateRedressalDto
+    {
+        public string Payload { get; set; }
+    }
+
+}
